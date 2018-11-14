8a6bc229d763b1bdfdd030d75d83eedb4fa6c52c
Jaydeep Chakrabarty <jaydeepc@thoughtworks.com>
2018-11-14 19:32:42 +0530

    JD: Added techstack graph and service to fetch git stats

--- open_hub/openhub/urls.py
+++ open_hub/openhub/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name='user'),
     url(r'^allprojects/$', views.projects, name='projects'),
     url(r'^api/techstack-data/$', views.get_techstack_count, name='api-data'),
-    url(r'^api/office-dist-data/$', views.OfficeDistribution.as_view())
+    url(r'^api/office-dist-data/$', views.OfficeDistribution.as_view()),
+    url(r'^api/git-stats/$', views.get_github_stats, name='git-data')
 ]
